26c2b6e114d1523c5bea1220abeb48b2cbf811e3
robinpm <dev@robinpm.com>
2021-08-17 14:13:54 -0400

    Added dev folder with react.js initialised

--- index-dev.php
+++ index-dev.php
@@ -1,47 +1,39 @@
 <!DOCTYPE html>
 
-<head>
-    <meta charset="UTF-8">
-    <title> Shared Expenses Input | <?php echo date('F Y'); ?> </title>
-    <link rel="stylesheet" href="style-dev.css">
-    <!-- Fontawesome CDN Link -->
-    <link rel="apple-touch-icon" href="images/zilent.png">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-</head>
+<?php include 'header-dev.php' ?>
 
-<body>
 
-    <div class="title-text">Shared Expenses Input | <?php echo date('F Y'); ?> </div>
-    <nav>
-        <a href="./index-dev.php">
-            <button>Home</button>
-        </a>
-        <a href="sqlviewer.php">
-            <button>Table Viewer</button>
-        </a>
-    </nav>
+<body class='bg-purple-900'>
 
-    <form action="./gogo.php" method="post">
-        <div class="input-div">
-            <input type="text" name="name" placeholder="Who are you?">
-        </div>
-        <div class="input-div">
-            <input id="today" type="date" name="date">
-        </div>
-        <div class="input-div">
-            <input type="text" name="description" placeholder="What did you buy?">
-        </div>
-        <div class="input-div">
-            <input type="number" step="0.01" id="price" pattern="^\d*(\.\d{0,2})?$" title="Insert a price! (00.000" name="amount" placeholder="How much was it?">
-            <i>$</i>
-        </div>
-        <div class="input-div">
+    <div class="flex justify-center">
+        <div class="">
+            <div class="justify-center">
+                <h1>Shared Expenses Input | <?php echo date('F Y'); ?> </h1>
 
-            <input type="text" name="notes" placeholder="Anything else?">
+                <div class="justify-center bg-blue-100 rounded mx-10">
+                    <form action="./gogo.php" method="post">
+                        <div class="mb-6">
+                            <input class="bg-white rounded-md border border-gray-200 p-3 focus:outline-none w-full" type="text" name="name" placeholder="Who are you?">
+                        </div>
+                        <div class="mb-6">
+                            <input class="bg-white rounded-md border border-gray-200 p-3 focus:outline-none w-full" id="today" type="date" name="date">
+                        </div>
+                        <div class="mb-6">
+                            <input class="bg-white rounded-md border border-gray-200 p-3 focus:outline-none w-full" type="text" name="description" placeholder="What did you buy?">
+                        </div>
+                        <div class="mb-6">
+                            <input class="bg-white rounded-md border border-gray-200 p-3 focus:outline-none w-full" type="number" step="0.01" id="price" pattern="^\d*(\.\d{0,2})?$" title="Insert a price! (00.000" name="amount" placeholder="How much was it?">
+                            <!-- <i>$</i> -->
+                        </div>
+                        <div class="mb-6">
+                            <input class="bg-white rounded-md border border-gray-200 p-3 focus:outline-none w-full" type="text" name="notes" placeholder="Anything else?">
+                        </div>
+                        <button class="py-3 px-12 bg-red-600 hover:bg-red-800 mr-5 rounded-md text-white text-lg focus:outline-none w-full" type="submit">Submit</button>
+
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="button">
-            <input type="submit" value="Submit">
-        </div>
-    </form>
+    </div>
 
 </body>
--- index.php
+++ index.php
@@ -5,47 +5,43 @@
 
 <body>
     <div class="container">
-        <div class="content"></div>
-        <div class="right-side">
-            <div class="topic-text">Shared Expenses Input | <?php echo date('F Y'); ?> </div>
-            <a href="./index.php">
-                <button>Home</button>
-            </a>
-            <a href="./sqlviewer.php">
-                <button>Table Viewer</button>
-            </a>
-            <!----------------------------------------------------------------------------------------->
-            <form action="./gogo.php" method="post">
-                <div class="input-box">
-                    <input type="text" name="name" placeholder="Who are you?">
+        <h1 class="topic-text">Shared Expenses Input | <?php echo date('F Y'); ?> </h1>
 
-                </div>
-                <!-- <input type="text" name="name" id="name"> -->
-                <div class="input-box">
-                    <!-- <label for="date">date:</label> -->
-                    <input id="today" type="date" name="date">
-                </div>
-                <div class="input-box">
-                    <!-- <label for="description">description:</label> -->
-                    <input type="text" name="description" placeholder="What did you buy?">
-                </div>
-                <div class="input-box">
-                    <!-- <label for="amount">amount:</label> -->
-                    <input type="number" step="0.01" id="price" pattern="^\d*(\.\d{0,2})?$" title="Insert a price! (00.000" name="amount" placeholder="How much was it?">
-                    <i>$</i>
-                </div>
-                <div class="input-box">
-                    <!-- <label for="notes">notes:</label> -->
-                    <input type="text" name="notes" placeholder="Anything else?">
-                </div>
-                <div class="button">
-                    <input type="submit" value="Submit">
-                </div>
-            </form>
-        </div>
+        <a href="./index.php">
+            <button>Home</button>
+        </a>
+        <a href="./sqlviewer.php">
+            <button>Table Viewer</button>
+        </a>
+        <!----------------------------------------------------------------------------------------->
+        <form action="./gogo.php" method="post">
+            <div class="input-box">
+                <input type="text" name="name" placeholder="Who are you?">
 
-    </div>
-    </div>
+            </div>
+            <!-- <input type="text" name="name" id="name"> -->
+            <div class="input-box">
+                <!-- <label for="date">date:</label> -->
+                <input id="today" type="date" name="date">
+            </div>
+            <div class="input-box">
+                <!-- <label for="description">description:</label> -->
+                <input type="text" name="description" placeholder="What did you buy?">
+            </div>
+            <div class="input-box">
+                <!-- <label for="amount">amount:</label> -->
+                <input type="number" step="0.01" id="price" pattern="^\d*(\.\d{0,2})?$" title="Insert a price! (00.000" name="amount" placeholder="How much was it?">
+                <i>$</i>
+            </div>
+            <div class="input-box">
+                <!-- <label for="notes">notes:</label> -->
+                <input type="text" name="notes" placeholder="Anything else?">
+            </div>
+            <div class="button">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+
     </div>
 </body>
 
